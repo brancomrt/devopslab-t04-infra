ed268d443656524e6fb4a9428140772ad76b8f10
Vagrant User <vagrant@workspace>
2022-03-05 14:13:30 -0300

    teste

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "genial-shuttle-343213"
-  location_id = "us-west1"
+  location_id = "us-central1"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-west1"
+  location = "us-central1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
