e80395a97b33c6be196db3bf6011d264a4cca41f
Vagrant User <vagrant@workspace>
2022-03-05 13:00:02 -0300

    API engine

--- providers.tf
+++ providers.tf
@@ -10,7 +10,12 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
+  project = "genial-shuttle-343213"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
 
+provider "google-beta" {
   project = "genial-shuttle-343213"
   region  = "us-west1"
   zone    = "us-west1-b"
