24e50123069cd66f98a21aa78eabcff6aca89685
Vagrant User <vagrant@workspace>
2022-03-05 12:22:40 -0300

    Terraform GCP

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.12.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "genial-shuttle-343213"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
